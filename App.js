f81bcde004a0b9252c65b09aa91b754e9c9ea462
Brian Serein <bserein22@gmail.com>
2022-02-22 18:48:33 -0500

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headerText}>You did it my man!</Text>
+      <Button title="Click me!"
+      accessibilityLabel='I just told you what to do'
+      onPress={() => Alert.alert('I just told you what to do')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'aqua',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerText:{
+    color: 'white',
+    fontSize: 36,
+    fontWeight: '900'
+  }
 });
